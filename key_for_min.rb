4ee2132a3d224ea7755dc3eef06e3ecb0b7d12de
Katie B <katiebittner1@gmail.com>
2020-09-08 18:17:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,8 @@ def key_for_min_value(name_hash)
   current_value = 0
   name_hash.each do |value|
     if value>current_value && value>min_value
-      
+      current_value = value
   end
-
+  min_value = current_value
+  min_value
 end
